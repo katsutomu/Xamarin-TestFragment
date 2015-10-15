96d2018ed471f190386f881a9314fdaf5aba1c75
katsunori sekine <katsutomu0124@gmail.com>
2015-10-16 01:02:36 +0900

    Add dummy tab

---
+++ TestFlagment/Views/HomeTabActivity.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Cirrious.MvvmCross.Droid.Fragging;
+
+namespace TestFlagment
+{
+	[Activity (Label = "HomeTabActivity")]			
+	public class HomeTabActivity : MvxTabsFragmentActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Create your application here
+		}
+
+		public TabView()
+			: base(Resource.Layout.Page_TabView, Resource.Id.actualtabcontent)
+		{
+		}
+	}
+}
+
